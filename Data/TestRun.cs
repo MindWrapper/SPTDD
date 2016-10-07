a23c92bffd99ab538a59423340d6ee94cfd2278d
Yan Drugalya <ydrugalya@gmail.com>
2016-10-07 08:50:19 +0200

    Initial structure for data module

---
+++ Data/TestRun.cs
@@ -0,0 +1,10 @@
+﻿namespace Data
+{
+	public class TestRun
+	{
+		public int Id { get; set; }
+		public string Test { get; set; }
+		public string BuildAgent { get; set; }
+		public bool Success { get; set; }
+	}
+}
