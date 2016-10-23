ca91501997e79d76563f37818e38a75244d28676
Yan Drugalya <ydrugalya@gmail.com>
2016-10-23 15:13:22 +0200

    Next test which, which checks that GetTestRun returns one stat entry if there is one in the database

--- Data.Tests/TestRunStatsTests.cs
+++ Data.Tests/TestRunStatsTests.cs
@@ -1,10 +1,17 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 
 namespace Data.Tests
 {
 	[TestFixture]
 	public class TestRunStatsTests
 	{
+		[SetUp]
+		public void Setup()
+		{
+			CleanupDatabase();
+		}
+
 		[Test]
 		public void GetRunStats_NoRunsInDatabase_ReturnsNothing()
 		{
@@ -12,6 +19,26 @@ namespace Data.Tests
 			var statsService = new TestRunStats(context);
 			var results = statsService.GetRunStats();
 			Assert.IsEmpty(results);
+		}
+
+		[Test]
+		public void GetRunStats_OneTestRunInDatabase_ReturnsStatsEntry()
+		{
+			var context = new TestDataDbContext("TestConnection");
+			var statsService = new TestRunStats(context);
+			var tr = new TestRun();
+			context.TestRuns.Add(tr);
+			context.SaveChanges();
+
+			var results = statsService.GetRunStats();
+
+			Assert.That(results.Count(), Is.EqualTo(1));
+		}
+
+		private static void CleanupDatabase()
+		{
+			var context = new TestDataDbContext("TestConnection");
+			context.Database.ExecuteSqlCommand("delete from TestRuns");
 		}
 	}
 }
